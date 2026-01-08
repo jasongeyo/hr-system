49c38ac0ed29c97fd6989120d6548e1f039b7f59
jasonko <f2675805@gmail.com>
2026-01-08 14:45:41 +0800

    Add GET /employees API with mock data

--- src/main/java/org/hr/hr/model/Employee.java
+++ src/main/java/org/hr/hr/model/Employee.java
@@ -5,4 +5,24 @@ public class Employee {
     private Long id;
     private String name;
     private String department;
+
+    public Employee(Long id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public Employee() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
 }
--- src/main/java/org/hr/hr/service/EmloyeeService.java
+++ src/main/java/org/hr/hr/service/EmloyeeService.java
@@ -1,7 +1,20 @@
 package org.hr.hr.service;
 
+import org.hr.hr.model.Employee;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EmloyeeService {
+
+    public List<Employee> findAll() {
+        List<Employee> list = new ArrayList<>();
+
+        list.add(new Employee(1L , "Jason" , "HR"));
+        list.add(new Employee(2L , "Amy" , "Finance"));
+
+        return list;
+    }
 }
